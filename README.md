71ed75d7ee9bb2d683e4cbe640c5b38ad20ed503
Do Hyun Nam <dollis2002@gmail.com>
2022-05-10 11:28:29 -0500

    Base template done

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 ## Referencias
 https://www.footprintcalculator.org/en/quiz/0/food/category
+https://www.winacc.org.uk/downloads/STAP/Shorter_Transport%20Emissions%20Report_110328.pdf
+https://www.revistaturbo.com/noticias/cuanto-carbono-producen-los-distintos-tipos-de-vehiculo-1197
+https://www.bbc.com/mundo/noticias-51020403
 # Emergencia-climatica
---
+++ main.js
@@ -0,0 +1,16 @@
+const numKg = document.getElementById("num-kg");
+const numKm = document.getElementById("num-km");
+const numPeople = document.getElementById("num-people");
+const resultRender = document.getElementById("results");
+
+const peopleChange = () => {
+  resultRender.innerHTML = `<p>${numPeople.value} people will need to drive ${numKm.value} km with ${numKg.value} kg</p>`;
+};
+
+const kmChange = () => {
+  resultRender.innerHTML = `<p>${numPeople.value} people will need to drive ${numKm.value} km with ${numKg.value} kg</p>`;
+};
+
+const kgChange = () => {
+  resultRender.innerHTML = `<p>${numPeople.value} people will need to drive ${numKm.value} km with ${numKg.value} kg</p>`;
+};
