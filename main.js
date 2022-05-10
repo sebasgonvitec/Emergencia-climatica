3d9ff28e628f49aabd8e3277070821d6f1f97a20
Do Hyun Nam <dollis2002@gmail.com>
2022-05-10 16:40:28 -0500

    Works

--- main.js
+++ main.js
@@ -3,23 +3,101 @@ const numKm = document.getElementById("num-km");
 const numPeople = document.getElementById("num-people");
 const resultRender = document.getElementById("results");
 
-const peopleChange = () => {
-  resultRender.innerHTML = `<p>${numPeople.value} people will need to drive ${numKm.value} km with ${numKg.value} kg</p>`;
+const vehicleData = [
+  {
+    name: "car",
+    emission: 120.1,
+    maxPeople: 5,
+    maxCargo: 400,
+    image:
+      "https://seetech-corp.com/wp-content/uploads/2016/10/Car-PNG-File.png",
+  },
+  {
+    name: "bus",
+    emission: 1300,
+    maxPeople: 40,
+    maxCargo: 1280,
+    image: "https://www.pngmart.com/files/6/Bus-Transparent-Images-PNG.png",
+  },
+  {
+    name: "van",
+    emission: 180.5,
+    maxPeople: 13,
+    maxCargo: 750,
+    image:
+      "https://vans.mercedes-benz.com.mx/vans/es/sprinter/quick-access/body-types/sprinter-tourer/_jcr_content/parsysmeta/meta/image.mq6.png/1647914985000.png",
+  },
+  {
+    name: "truck",
+    emission: 1540,
+    maxPeople: 2,
+    maxCargo: 24000,
+    image: "https://www.picng.com/upload/truck/png_truck_23632.png",
+  },
+  {
+    name: "plane",
+    emission: 115.4,
+    maxPeople: 150,
+    maxCargo: 4800,
+    image:
+      "https://www.pngmart.com/files/13/Vector-Flying-Airplane-PNG-Free-Download.png",
+  },
+];
+
+const valueChange = () => {
+  const people = numPeople.value;
+  const km = numKm.value;
+  const kg = numKg.value;
+
+  const suggestion = returnSuggestion(people, km, kg);
+  console.log(suggestion);
+
+  let addString = "";
+
+  suggestion.forEach((vehicle) => {
+    addString += `<div class="col-md-4 col-sm-12 flexer-col">
+    <img src=${vehicle.image} alt="" width="100" />
+    <h3>${vehicle.name}</h3>
+    <p>Se ocupan <strong>${vehicle.total}</strong> flete(s)</p>
+    <p>Su emision de CO2 total por el viaje es de <strong>${vehicle.totalEmissions}</strong></p>
+  </div>`;
+  });
+
+  resultRender.innerHTML = addString;
 };
 
-const kmChange = () => {
-  resultRender.innerHTML = `<p>${numPeople.value} people will need to drive ${numKm.value} km with ${numKg.value} kg</p>`;
+const calculateEmission = (vehicles, km, em) => {
+  const emissions = vehicles * km * em;
+  return emissions;
 };
 
-const kgChange = () => {
-  resultRender.innerHTML = `<p>${numPeople.value} people will need to drive ${numKm.value} km with ${numKg.value} kg</p>`;
+const howMuchWeNeed = (maxPeople, maxCargo, people, cargo) => {
+  totalVehiclesByCargo = Math.ceil(cargo / maxCargo);
+  totalVehiclesByPeople = Math.ceil(people / maxPeople);
+
+  return Math.max(totalVehiclesByCargo, totalVehiclesByPeople);
 };
 
-const calculateEmission = (people, km, em) => {
-    const emissions = people * km * em;
-    return emissions;
-}
+const returnSuggestion = (people, km, kg) => {
+  let vehicles = [];
 
+  vehicleData.forEach((vehicle) => {
+    const totalVehicles = howMuchWeNeed(
+      vehicle.maxPeople,
+      vehicle.maxCargo,
+      people,
+      kg
+    );
 
+    if (totalVehicles > 0) {
+      vehicles.push({
+        name: vehicle.name,
+        total: totalVehicles,
+        totalEmissions: calculateEmission(totalVehicles, km, vehicle.emission),
+        image: vehicle.image,
+      });
+    }
+  });
 
-
+  return vehicles;
+};
